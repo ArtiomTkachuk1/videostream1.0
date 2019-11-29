baa1e19db3ce3ecacb51af8e644a9658bd4a5317
ArtiomTkachuk1 <32066519+ArtiomTkachuk1@users.noreply.github.com>
2019-11-29 19:30:08 +0200

    -

---
+++ vidtest/build/precache-manifest.5a33a0a6905b52f376a4ebd810a7902f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76eefc1e4b1d02ac5fc98a89e6759346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6a3a7b211017b2e7686",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3cddd42ddd8c69af7b93",
+    "url": "/static/js/2.5ff29494.chunk.js"
+  },
+  {
+    "revision": "d6a3a7b211017b2e7686",
+    "url": "/static/js/main.a6e60fd7.chunk.js"
+  },
+  {
+    "revision": "a0c4142ab97e42587641",
+    "url": "/static/js/runtime-main.9ac3fe16.js"
+  }
+]);
